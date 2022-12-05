64745f1be6c3613f37991aa6061cd09bd049b294
Ng Yong Zhi <41257178+ngyz96@users.noreply.github.com>
2022-12-05 15:00:49 +0800

    Update qiime2_deblur.sh

--- scripts/its/tembusu/qiime2_deblur.sh
+++ scripts/its/tembusu/qiime2_deblur.sh
@@ -3,7 +3,7 @@
 module load anaconda2020/python3
 eval "$(conda shell.bash hook)"
 conda activate /usr/local_sbs/conda_microbes
-PWD="/home/bmb/Soil-Microbiomes/Tembusu-Microbiome/qiime2_its"
+PWD="Soil-Microbiomes/Tembusu-Microbiome/qiime2_its"
 qimme quality filter q-score \
 --i-demux $PWD/its_paired_end.qza \
 --o-filtered-sequences its_paired_end_filtered.qza \
