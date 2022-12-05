f1a391112434806188860a050fe611348409f101
Ng Yong Zhi <41257178+ngyz96@users.noreply.github.com>
2022-12-05 15:01:34 +0800

    Update qiime2_itsexpress.sh

--- scripts/its/tembusu/qiime2_itsexpress.sh
+++ scripts/its/tembusu/qiime2_itsexpress.sh
@@ -4,9 +4,9 @@ eval "$(/usr/local/anaconda3-2020/bin/conda shell.bash hook)"
 conda activate /usr/local_sbs/conda_microbes
 
 qiime itsxpress trim-pair-output-unmerged \
---i-per-sample-sequences /home/bmb/Soil-Microbiomes/Tembusu-Microbiome/qiime2_its/its_paired_end.qza \
+--i-per-sample-sequences /Soil-Microbiomes/Tembusu-Microbiome/qiime2_its/its_paired_end.qza \
 --p-region ITS2 \
 --p-taxa F \
 --p-cluster-id 1.0 \
 --p-threads 12 \
---o-trimmed /home/bmb/Soil-Microbiomes/Tembusu-Microbiome/qiime2_its/its_paired_end_trimmed_unmerged.qza
+--o-trimmed /Soil-Microbiomes/Tembusu-Microbiome/qiime2_its/its_paired_end_trimmed_unmerged.qza
